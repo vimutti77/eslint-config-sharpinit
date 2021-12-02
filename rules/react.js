3c5c65a078d61ef4aea7ac2c92a744fc41bae02d
vimutti <vimutti@outlook.com>
2021-12-02 16:08:24 +0700

    Disable react/function-component-definition

--- rules/react.js
+++ rules/react.js
@@ -1,6 +1,7 @@
 module.exports = {
   'react/destructuring-assignment': 'off',
   'react/display-name': 'off',
+  "react/function-component-definition": "off",
   'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
   'react/jsx-props-no-spreading': 'off',
   'react/react-in-jsx-scope': 'off',
