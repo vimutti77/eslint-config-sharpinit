1eeb53a77cb43fdcf93237f39b8bf32a8eb24e3b
vimutti <vimutti@outlook.com>
2022-05-02 14:03:07 +0700

    only use jsx-a11y/anchor-is-valid for native anchor

--- common/rules/jsx-a11y.js
+++ common/rules/jsx-a11y.js
@@ -1,3 +1,9 @@
 module.exports = {
   'jsx-a11y/label-has-associated-control': 'off',
+  'jsx-a11y/anchor-is-valid': [
+    'warn',
+    {
+      aspects: ['noHref', 'invalidHref', 'preferButton'],
+    },
+  ],
 }
