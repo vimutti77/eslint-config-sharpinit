5d2fb42397148b27d5631deb5322d3664c355095
vimutti <vimutti@outlook.com>
2022-06-13 23:36:30 +0700

    enable react/jsx-key

--- packages/eslint-rules/src/react.js
+++ packages/eslint-rules/src/react.js
@@ -1,9 +1,17 @@
 module.exports = {
   'react/destructuring-assignment': 'off',
   'react/display-name': 'off',
-  "react/function-component-definition": "off",
+  'react/function-component-definition': 'off',
+  'react/jsx-key': [
+    'warn',
+    {
+      checkFragmentShorthand: true,
+      checkKeyMustBeforeSpread: true,
+      warnOnDuplicates: true,
+    },
+  ],
   'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
-  "react/jsx-no-useless-fragment": ['warn', { allowExpressions: true }],
+  'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
   'react/jsx-props-no-spreading': 'off',
   'react/react-in-jsx-scope': 'off',
   'react/require-default-props': 'off',
