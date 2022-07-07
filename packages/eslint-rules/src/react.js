518115677b84d03b1d6f4a55b3fc3527342822e6
vimutti <vimutti@outlook.com>
2022-07-07 11:26:23 +0700

    disable no-array-index-key

--- packages/eslint-rules/src/react.js
+++ packages/eslint-rules/src/react.js
@@ -13,7 +13,8 @@ module.exports = {
   'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
   'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
   'react/jsx-props-no-spreading': 'off',
+  'react/no-array-index-key': 'off',
+  'react/prop-types': 'off',
   'react/react-in-jsx-scope': 'off',
   'react/require-default-props': 'off',
-  'react/prop-types': 'off',
 }
