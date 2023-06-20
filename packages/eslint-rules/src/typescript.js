dafdead895a848ee64bdce9e36d9e9345aea1830
vimutti <vimutti@outlook.com>
2023-06-20 18:21:42 +0700

    allow @typescript-eslint/no-use-before-define for some case

--- packages/eslint-rules/src/typescript.js
+++ packages/eslint-rules/src/typescript.js
@@ -3,4 +3,13 @@ module.exports = {
   '@typescript-eslint/naming-convention': 'off',
   '@typescript-eslint/no-unnecessary-condition': 'warn',
   '@typescript-eslint/no-unused-vars': 'warn',
+  '@typescript-eslint/no-use-before-define': [
+    'error',
+    {
+      functions: false,
+      classes: true,
+      variables: false,
+      allowNamedExports: false,
+    },
+  ],
 }
